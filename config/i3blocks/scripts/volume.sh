530602b4dc2df115b64ad04458033a32024f0813
Fajar Hari Prabowo <me.fhprabowo@gmail.com>
2022-02-07 08:14:52 +0700

    adding some widget to i3blocks

--- config/i3blocks/scripts/volume.sh
+++ config/i3blocks/scripts/volume.sh
@@ -3,6 +3,6 @@ MUTE_STAT=$(pulsemixer --get-mute)
 GET_VOLUME=$(pulsemixer --get-volume | awk '{print $1}')
 
 if [ $MUTE_STAT -eq 1 ]
-    then echo "  muted"
+    then echo "  muted "
     else echo "  $GET_VOLUME% "
 fi
